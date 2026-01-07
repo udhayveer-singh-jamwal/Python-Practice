195688e64044a6431ddf51dd1a572be139e84c2e
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-01-07 23:04:27 +0530

    Add file writing and appending examples

--- 02_intermediate/01_file_handling.py
+++ 02_intermediate/01_file_handling.py
@@ -1,1 +1,7 @@
+# Write file
+with open("data.txt", "w") as f:
+    f.write("Learning Python File Handling\n")
 
+# Append file
+with open("data.txt", "a") as f:
+    f.write("Intermediate Level\n")
