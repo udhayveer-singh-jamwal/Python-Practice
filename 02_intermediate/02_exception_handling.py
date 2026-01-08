fa0c69b3e35eb05b1c96a2ce0dbd3610dbbd7279
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-01-08 17:53:23 +0530

    Add exception handling for user input and division

--- 02_intermediate/02_exception_handling.py
+++ 02_intermediate/02_exception_handling.py
@@ -1,1 +1,11 @@
-
+try:
+    a = int(input("Enter number: "))
+    print(100 / a)
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+except ValueError:
+    print("Invalid input")
+else:
+    print("No error")
+finally:
+    print("Program ended")
