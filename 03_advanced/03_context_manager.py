314f86f6ef9b2a0b0af2b926717344f82940394e
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-01-16 22:30:54 +0530

    Implement __exit__ method in FileManager context manager
    Added __exit__ method to FileManager for proper resource cleanup.

--- 03_advanced/03_context_manager.py
+++ 03_advanced/03_context_manager.py
@@ -4,3 +4,9 @@ class FileManager:
 
     def __enter__(self):
         return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+with FileManager("test.txt") as f:
+    f.write("Using context manager")
