927fbfb25c20d57c1aae0aebae00dd98f0e62997
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-01-21 22:44:14 +0530

    Update 05_large_small.py

--- 04_advance_practice/05_large_small.py
+++ 04_advance_practice/05_large_small.py
@@ -1,1 +1,13 @@
+nums = [45, 12, 89, 33, 67]
 
+largest = nums[0]
+smallest = nums[0]
+
+for n in nums:
+    if n > largest:
+        largest = n
+    if n < smallest:
+        smallest = n
+
+print("Largest:", largest)
+print("Smallest:", smallest)
