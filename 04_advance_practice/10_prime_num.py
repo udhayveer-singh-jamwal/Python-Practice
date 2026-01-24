0b3b91993262599ea2b15df5327d4370a36d6db3
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-01-24 22:14:15 +0530

    Update 10_prime_num.py

--- 04_advance_practice/10_prime_num.py
+++ 04_advance_practice/10_prime_num.py
@@ -1,1 +1,12 @@
+num = int(input("Enter number: "))
+is_prime = True
 
+if num <= 1:
+    is_prime = False
+
+for i in range(2, num):
+    if num % i == 0:
+        is_prime = False
+        break
+
+print("Prime" if is_prime else "Not Prime")
