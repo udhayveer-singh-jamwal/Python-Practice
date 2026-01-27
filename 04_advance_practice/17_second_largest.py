df22beda4ebda8be7e7de55b12672cd8231756bc
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-01-27 22:49:43 +0530

    Update 17_second_largest.py

--- 04_advance_practice/17_second_largest.py
+++ 04_advance_practice/17_second_largest.py
@@ -1,1 +1,10 @@
+nums = [10, 40, 20, 50, 30]
+largest = second = nums[0]
 
+for n in nums:
+    if n > largest:
+        second = largest
+        largest = n
+    elif n > second and n != largest:
+        second = n
+print("Second Largest:", second)
